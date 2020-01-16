9abe5ffb948131f8e863748961676ab4a2e32532
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-01-16 12:50:28 -0600

    feat(contact): add modified contact form from narative.co

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,14 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
 import Section from "@narative/gatsby-theme-novela/src/components/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
 import Headings from "@narative/gatsby-theme-novela/src/components/Headings";
+import mediaqueries from "@narative/gatsby-theme-novela/src/styles/media";
+
+
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
   return (
@@ -11,11 +16,26 @@ const Contact = () => {
       <SEO title="Contact - Student Dev Coach" />
       <Section>
         <div style={{ marginTop: "100px" }}>
-          <Headings.h1>Contact Me</Headings.h1>
+          <HeadingContainer>
+            <Headings.h1>Contact Me</Headings.h1>
+          </HeadingContainer>
+          <ContactForm />
         </div>
       </Section>
     </Layout>
   )
 }
+
+const HeadingContainer = styled.div`
+  margin: 100px 0;
+
+  ${mediaqueries.desktop`
+    width: 80%;
+  `}
+
+  ${mediaqueries.tablet`
+    width: 100%;
+  `}
+`;
 
 export default Contact;
